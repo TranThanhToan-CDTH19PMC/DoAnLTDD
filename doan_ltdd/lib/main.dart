535f372a09bbe11d7a574853b4a28f94093d5800
Trần Quang Thiện <0306191468@caothang.edu.vn>
2022-01-20 23:12:17 +0700

    Tran Quang Thien

--- doan_ltdd/lib/main.dart
+++ doan_ltdd/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Đăng Nhập',
+      title: 'Đăng Nhập ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
